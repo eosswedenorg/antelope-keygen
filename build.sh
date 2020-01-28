693c7aa06951e586cac98242c2bc22927c808097
Henrik Hautakoski <henrik@eossweden.org>
2020-01-28 12:31:13 +0100

    build.sh: handle build type option to cmake.

--- build.sh
+++ build.sh
@@ -2,6 +2,26 @@
 
 mkdir build 2> /dev/null
 pushd build > /dev/null
-cmake .. $@
+
+function usage() {
+	echo "Usage: ${0##*/} [ -h ] [ -t Debug|Release|RelWithDebInfo|MinSizeRel ]"
+	exit 1
+}
+
+ARGS=""
+while getopts ":h?t:" opt; do
+
+	case "${opt}" in
+		t) 	ARGS="${ARGS} -DCMAKE_BUILD_TYPE=${OPTARG}" ;;
+		h)  usage ;;
+		:) 	echo "Error: -${OPTARG} requires a value"
+			exit 1
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
+cmake $ARGS $@ ..
 make -B
+
 popd > /dev/null
